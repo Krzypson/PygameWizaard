2eac9954c3be6afd92c2b4e4d543afc1c37ed23b
Krzypson <wiktormroczek2004@gmail.com>
2023-05-26 01:01:58 +0200

    Fireballs
    Worked on the fireballs a bit + added an icon so the folder is prettier :))

--- test1.py
+++ test1.py
@@ -8,7 +8,8 @@ pg.init()
 width = 800
 height = 600
 window = pg.display.set_mode((width,height),pg.RESIZABLE)
-pg.display.set_caption("nienawidzę kobiet")
+pg.display.set_caption("Czarodziej lmao")
+pg.display.set_icon(pg.image.load(os.path.join('Assets','wizhat.png')))
 clock=pg.time.Clock()
 
 #wizard
@@ -21,7 +22,7 @@ wizard = pg.Rect(50,250,wizard_width,wizard_height)
 fireball_sprite = pg.image.load(os.path.join('Assets','fireball.png'))
 fireball_width,fireball_height = 100,100
 fireball_graph = pg.transform.scale(fireball_sprite,(fireball_width,fireball_height))
-fireball = pg.Rect(500,250,fireball_width,fireball_height)
+fireball = pg.Rect(0,0,fireball_width,fireball_height)
 
 def DrawBack():
     window.fill((119,241,245))
@@ -30,32 +31,40 @@ def DrawWiz(wizard):
 def DrawFireball(fireball):
     window.blit(fireball_graph,(fireball.x,fireball.y))
     pg.display.update()
-    
 def Draw():
     DrawBack()
     DrawWiz(wizard)
     pg.display.update()
 
-def WizardAction():
+def Fireball(a):
+    pg.time.set_timer()
+
+    
+    
+
+def WizardMovement():
     wizard_movespeed=4
     keys_pressed = pg.key.get_pressed()
     if(keys_pressed[pg.K_w]):wizard.y-=wizard_movespeed
     if(keys_pressed[pg.K_s]):wizard.y+=wizard_movespeed
     if(keys_pressed[pg.K_a]):wizard.x-=wizard_movespeed
     if(keys_pressed[pg.K_d]):wizard.x+=wizard_movespeed
-    if(keys_pressed[pg.K_SPACE]):DrawFireball(fireball)
-        
 
 def main():
+    FB=0
     running = True
     while running:
         clock.tick(60)
         for event in pg.event.get():
             if(event.type == pg.QUIT):
                 running = False
-
+            if(event.type == pg.KEYDOWN):
+                if(event.key == pg.K_SPACE):FB=200
+        WizardMovement()
         Draw()
-        WizardAction()
+        if(FB>0):
+            DrawFireball(fireball)
+            FB-=1
 
 if __name__=="__main__":
     main()
