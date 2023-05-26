f19a33bb8b814dbfbd690849cfaf0abc088c5ad6
Krzypson <wiktormroczek2004@gmail.com>
2023-05-27 00:49:05 +0200

    added non funcional UI an non functional enemies

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import os
+import random
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
@@ -11,12 +12,15 @@ window = pg.display.set_mode((width,height),pg.RESIZABLE)
 pg.display.set_caption("Czarodziej lmao")
 pg.display.set_icon(pg.image.load(os.path.join('Assets','wizhat.png')))
 clock=pg.time.Clock()
+screen_width = pg.display.Info().current_w
+screen_height = pg.display.Info().current_h
 
 #wizard
 wizard_sprite = pg.image.load(os.path.join('Assets','wizard.png'))
 wizard_width,wizard_height = 100,100
 wizard_graph = pg.transform.scale(wizard_sprite,(wizard_width,wizard_height))
 wizard = pg.Rect(50,250,wizard_width,wizard_height)
+hearts = 3
 
 #fireball
 Fireballs=[]
@@ -25,22 +29,48 @@ fireball_width,fireball_height = 100,100
 fireball_graph = pg.transform.scale(fireball_sprite,(fireball_width,fireball_height))
 fireball_movespeed=8
 
+#goblin
+Goblins=[]
+goblin_sprite = pg.image.load(os.path.join('Assets','goblin.png'))
+goblin_width,goblin_height=45,75
+goblin_graph = pg.transform.scale(goblin_sprite,(goblin_width,goblin_height))
+
 #border
 border_left = 0
-border_right = pg.display.Info().current_w - wizard_width
+border_right = screen_width - wizard_width
 border_top = 0
-border_bottom = pg.display.Info().current_h - wizard_height
+border_bottom = screen_height - wizard_height
+
+#UI
+heart_icon = pg.image.load(os.path.join('Assets','hearticon.png'))
+fireball_icon = pg.image.load(os.path.join('Assets','fireballicon.png'))
 
 def DrawBack():
     window.fill((119,241,245))
 def DrawWiz():
     window.blit(wizard_graph,(wizard.x,wizard.y))
+def DrawGoblins():
+    for gob in Goblins:
+        window.blit(goblin_graph,(gob.x,gob.y))
 def DrawFireball():
     for fball in Fireballs:
         window.blit(fireball_graph,(fball.x,fball.y))
+def DrawUi():
+    match hearts:
+        case 1:
+            window.blit(heart_icon,(10,10))
+        case 2:
+            window.blit(heart_icon,(10,10))
+            window.blit(heart_icon,(20+heart_icon.get_width(),10))
+        case 3:
+            window.blit(heart_icon,(10,10))
+            window.blit(heart_icon,(15+heart_icon.get_width(),10))
+            window.blit(heart_icon,(20+2*heart_icon.get_width(),10))
 def Draw():
     DrawBack()
     DrawWiz()
+    DrawGoblins()
+    DrawUi()
     DrawFireball()
     pg.display.update()
 
@@ -59,9 +89,21 @@ def FireballHandling():
                 fball.x+=fireball_movespeed
             else:
                 Fireballs.remove(fball)
+                
+def SpawnGoblins():
+    global Goblins
+    if(len(Goblins)<5):
+        goblin = pg.Rect(random.randint(3*screen_width//4,screen_width),random.randint(0,screen_height),goblin_width,goblin_height)
+        Goblins.append(goblin)
+        
+def GoblinMovement():
+    global Goblins
+    for goblin in Goblins:
+        goblin.x-=1
 
 def main():
-    FB=0
+    screen_width = pg.display.Info().current_w
+    screen_height = pg.display.Info().current_h
     running = True
     while running:
         clock.tick(60)
@@ -72,6 +114,8 @@ def main():
                 if(event.key == pg.K_SPACE):
                     fireball = pg.Rect(wizard.x,wizard.y,fireball_width,fireball_height)
                     Fireballs.append(fireball)
+        SpawnGoblins()
+        GoblinMovement()
         WizardMovement()
         Draw()
         FireballHandling()
